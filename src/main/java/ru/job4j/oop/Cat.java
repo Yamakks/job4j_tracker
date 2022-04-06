482601547efd2ff65cb7d4ce8c6c9b76f08edc1e
Yamakks <maxim5_91@mail.ru>
2022-04-06 14:00:22 +0300

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -6,10 +6,36 @@ public class Cat {
         return voice;
     }
 
+    private String food;
+
+    private String name;
+
+    public void show() {
+        System.out.println(this.name + " съел " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
     public static void main(String[] args) {
         Cat peppy = new Cat();
-        Cat sparky = new Cat();
         String say = peppy.sound();
         System.out.println("Peppy says " + say);
+        System.out.println();
+        System.out.println("Котенок гав.");
+        Cat gav = new Cat();
+        gav.giveNick("Гав");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("Котенок блэк.");
+        Cat black = new Cat();
+        black.giveNick("Черный");
+        black.eat("fish");
+        black.show();
     }
 }
