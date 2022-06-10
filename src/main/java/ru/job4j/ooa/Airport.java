5463a9c83819a144de5c1d5e67d7bb44b1976c41
Yamakks <maxim5_91@mail.ru>
2022-06-10 13:49:18 +0300

    2. final [#334073]

---
+++ src/main/java/ru/job4j/ooa/Airport.java
@@ -0,0 +1,16 @@
+package ru.job4j.ooa;
+
+public class Airport {
+    public static void main(String[] args) {
+        final Airbus airbus = new Airbus("A320");
+        System.out.println(airbus);
+        airbus.printModel();
+        airbus.printCountEngine();
+        Airbus airbus1 = new Airbus("A385");
+        System.out.println(airbus1);
+        airbus1.printCountEngine();
+        airbus.setName("A380");
+        System.out.println(airbus);
+        airbus.printCountEngine();
+    }
+}
