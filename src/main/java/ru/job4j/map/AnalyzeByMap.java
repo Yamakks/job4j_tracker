f4330022124ddd78c26560ffbe5619c3b5e35e29
Yamakks <maxim5_91@MAIL/RU>
2022-08-29 00:51:32 +0300

    4. Аттестация [#504878]

--- src/main/java/ru/job4j/map/AnalyzeByMap.java
+++ src/main/java/ru/job4j/map/AnalyzeByMap.java
@@ -1,4 +1,49 @@
 package ru.job4j.map;
 
+import java.util.List;
+
 public class AnalyzeByMap {
+    public static double averageScore(List<Pupil> pupils) {
+        int rsl = 0;
+        int sum = 0;
+        for (Pupil pupil : pupils) {
+           for (Subject subject : pupil.subjects()) {
+               rsl += subject.score();
+               sum++;
+           }
+        }
+        return (double) rsl / sum;
+    }
+
+    public static List<Label> averageScoreByPupil(List<Pupil> pupils) {
+       double score = 0D;
+       int index = 0;
+       int indexOfScore = 0;
+        Label[] rsl = new Label[pupils.size()];
+        for (Pupil pupil : pupils) {
+            indexOfScore = 0;
+            for (Subject subject : pupil.subjects()) {
+                score *= indexOfScore;
+                indexOfScore = 1;
+                score += (double) subject.score() / pupil.subjects().size();
+            }
+            rsl[index] = new Label(pupil.name(), score);
+            index++;
+        }
+
+        return List.of(rsl);
+    }
+
+    public static List<Label> averageScoreBySubject(List<Pupil> pupils) {
+        return List.of();
+    }
+
+    public static Label bestStudent(List<Pupil> pupils) {
+        return null;
+    }
+
+    public static Label bestSubject(List<Pupil> pupils) {
+        return null;
+    }
+
 }
