4b91028605e8b78801f5e19475be1c66e937cff6
Yamakks <maxim5_91@mail.ru>
2022-09-01 13:10:16 +0300

    1. Организовать сортировку User [#10034 #337891]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -33,6 +33,7 @@ public class User implements Comparable<User> {
 
     @Override
     public int compareTo(User o) {
-        return name.compareTo(o.name) == 0 ? Integer.compare(age, o.age) : name.compareTo(o.name);
+        int result = name.compareTo(o.name);
+        return result == 0 ? Integer.compare(age, o.age) : result;
     }
 }
