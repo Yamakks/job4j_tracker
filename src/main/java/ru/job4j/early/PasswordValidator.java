cc42035678f80ba7d3a37c0169a26145c0bcee1a
maxim <maxim5_91@mail.ru>
2022-11-24 02:02:30 +0300

    4. Принципы раннего возврата и охранных выражений [#504875]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -3,33 +3,49 @@ package ru.job4j.early;
 import java.util.Locale;
 
 public class PasswordValidator {
-    @SuppressWarnings("checkstyle:NoWhitespaceBefore")
+
     public static String validate(String password) {
+        boolean checkDigit = false;
+        boolean checkSymbol = false;
 
         if (password == null) {
             throw new IllegalArgumentException("Password can't be null");
         }
-        String buf1 =password.toLowerCase();
-        String buf2 = password.toUpperCase();
-        if (password.length() <= 8 || password.length() >= 32) {
+        if (password.length() < 8 || password.length() > 32) {
             throw new IllegalArgumentException("Password should be length [8, 32]");
         }
-            if (buf1.equals(password)) {
-                throw new IllegalArgumentException("Password should contain at least one uppercase letter");
-            }
-            if (buf2.equals(password)) {
-                throw new IllegalArgumentException("Password should contain at least one lowercase letter");
-            }
-            if (password == null) {
-                throw new IllegalArgumentException("Password can't be null");
-            }
-            if (password == null) {
-                throw new IllegalArgumentException("Password can't be null");
-            }
-            if (password == null) {
-                throw new IllegalArgumentException("Password can't be null");
+        if (password.toLowerCase().equals(password)) {
+            throw new IllegalArgumentException("Password should contain at least one uppercase letter");
+        }
+        if (password.toUpperCase().equals(password)) {
+            throw new IllegalArgumentException("Password should contain at least one lowercase letter");
+        }
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isDigit(password.charAt(i))) {
+                checkDigit = true;
+                break;
             }
         }
-
+        if (!checkDigit) {
+            throw new IllegalArgumentException("Password should contain at least one figure");
+        }
+        for (int i = 0; i < password.length(); i++) {
+            if (!Character.isLetterOrDigit(password.charAt(i))) {
+                checkSymbol = true;
+                break;
+            }
+        }
+        if (!checkSymbol) {
+            throw new IllegalArgumentException("Password should contain at least one special symbol");
+        }
+        if (password.toLowerCase().contains("qwerty")
+                || password.contains("12345")
+                || password.toLowerCase().contains("password")
+                || password.toLowerCase().contains("admin")
+                || password.toLowerCase().contains("user")) {
+            throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
+        }
+        return password;
     }
 }
+
