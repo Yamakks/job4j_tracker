0e1b8af4b062cc70ab0aa82a9093806874723747
Yamakks <maxim5_91@mail.ru>
2022-11-23 16:04:14 +0300

    4. Принципы раннего возврата и охранных выражений [#504875]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -1,22 +1,24 @@
 package ru.job4j.early;
 
+import java.util.Locale;
+
 public class PasswordValidator {
     @SuppressWarnings("checkstyle:NoWhitespaceBefore")
     public static String validate(String password) {
+
         if (password == null) {
             throw new IllegalArgumentException("Password can't be null");
         }
+        String buf1 =password.toLowerCase();
+        String buf2 = password.toUpperCase();
         if (password.length() <= 8 || password.length() >= 32) {
             throw new IllegalArgumentException("Password should be length [8, 32]");
         }
-        for (int i = 0; i < password.length(); i++) {
-
-            if ((password.charAt(i)).isUpperCase) {
-                break;
-                throw new IllegalArgumentException("Password can't be null");
+            if (buf1.equals(password)) {
+                throw new IllegalArgumentException("Password should contain at least one uppercase letter");
             }
-            if (password == null) {
-                throw new IllegalArgumentException("Password can't be null");
+            if (buf2.equals(password)) {
+                throw new IllegalArgumentException("Password should contain at least one lowercase letter");
             }
             if (password == null) {
                 throw new IllegalArgumentException("Password can't be null");
