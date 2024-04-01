6ab7fd5e546bece9cc9afeba298e2f07cc61def9
maxim <maxim5_91@mail.ru>
2024-04-01 23:30:11 +0300

    2. Трекер SQL [#1734]

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -1,9 +1,8 @@
 package ru.job4j.tracker;
 
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Properties;
 
@@ -24,14 +23,23 @@ public class SqlTracker implements Store {
                 .getResourceAsStream("app.properties")) {
             Properties config = new Properties();
             config.load(input);
-            Class.forName(config.getProperty("driver-class-name"));
+            Class.forName(config.getProperty("driver"));
             connection = DriverManager.getConnection(
                     config.getProperty("url"),
-                    config.getProperty("username"),
+                    config.getProperty("login"),
                     config.getProperty("password")
             );
         } catch (Exception e) {
             throw new IllegalStateException(e);
+        }
+    }
+
+    private void doStatement(String query) throws SQLException {
+        try (Statement statement = connection.createStatement()) {
+            if (query.startsWith("CREATE")) {
+                statement.execute(query);
+            }
+            statement.executeUpdate(query);
         }
     }
 
@@ -44,17 +52,37 @@ public class SqlTracker implements Store {
 
     @Override
     public Item add(Item item) {
-        return null;
+        try (PreparedStatement preparedStatement =
+                     connection.prepareStatement("INSERT INTO items(name, created) VALUES (?, ?)")) {
+            preparedStatement.setString(1, item.getName());
+            preparedStatement.setTimestamp(2, Timestamp.valueOf(item.getCreated()));
+            preparedStatement.execute();
+
+            try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    long id = generatedKeys.getLong(1);
+                    item.setId((int) id);
+                } else {
+                    throw new SQLException("Сгенерированный ключ не найден.");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return item;
     }
 
-    @Override
+            @Override
     public boolean replace(int id, Item item) {
         return false;
     }
 
     @Override
-    public void delete(int id) {
+    public boolean delete(int id) {
 
+        return false;
     }
 
     @Override
